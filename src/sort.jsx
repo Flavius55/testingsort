b9b36cf74a5dc8f5f046de575e6d4ff13e12285f
unknown <catalin_vl67@yahoo.com>
2020-04-13 00:43:37 +0300

    ziuaa1

--- src/sort.jsx
+++ src/sort.jsx
@@ -3,8 +3,12 @@ import './sort.css';
 
 let vector = [];
 let compara = [];
-let lungime=50;
 let maxim=100;
+
+let wheight = window.innerHeight;
+let wwidth  = window.innerWidth;
+
+let lungime = (wwidth*7/10)/(wwidth/200*3) ;
 
 function generateNewArray()
 {
@@ -12,11 +16,18 @@ function generateNewArray()
     {
         vector[i] = randomInteger(5, maxim);
     }
+    let r=randomInteger(5,lungime-5);
+    vector[r] = maxim-1;
 }
 
 function randomInteger(min,max)
 {
     return Math.floor(Math.random()*(max-min)+min);
+}
+
+function distantier()
+{
+    document.getElementsByClassName("bars")[0].style.marginLeft="5vw";
 }
 
 class Element extends React.Component
@@ -28,17 +39,37 @@ class Element extends React.Component
     render()
     {
         generateNewArray();
-        for(let i=0;i<lungime;i++) console.log(vector[i]);
+        console.log(window.height);
+        console.log(window.width);
         return(
             <div className="grafic">
                 {
                 vector.map((value,index) =>
-                ( <div className="bars" style={{height: `${value}px`}} key={index}></div>
+                ( <div className="bars" 
+                        style={{height: `${Math.floor(value*((wheight)*65/100)/100)}px`}} 
+                        key={index}></div>
                 ))
                 }
+                <button className="buttoN" onClick={reset()}></button>
             </div>
         );
     }
+}
+
+window.onload = function()
+{
+    distantier();
+    //console.log(Math.floor(wheight*65/100)-1);
+}
+
+function reset()
+{
+    generateNewArray();
+    /*let bar = document.getElementsByClassName("bars");
+    for(let i=0;i<lungime;i++)
+    {
+        bar[i].style.height = vector[i]*(((wheight)*65/100)/100)+'px';
+    }*/
 }
 
 export default Element;
