20dd169e72caa8078c601b1e9fc597718bbaed55
unknown <catalin_vl67@yahoo.com>
2020-04-14 00:30:59 +0300

    ziua3

--- src/sort.jsx
+++ src/sort.jsx
@@ -45,8 +45,11 @@ class Element extends React.Component
                 ))
                 }
                 </div>
-                <button className="buttN" onClick={(e) => reset()}></button>
-                <button className="buttoN" onClick={(e) => BubbleSort()}></button>
+                <button className="buttN" onClick={(e) => reset()}>reset</button>
+                <button className="buttoN" onClick={(e) => BubbleSort()}>bubble</button>
+                <button className="bmerge" 
+                onClick={(e) => MergeSort()}>merge</button>
+                <button className="afis" onClick={(e) => afis()}>afis</button>
             </div>
         );
     }
@@ -54,8 +57,8 @@ class Element extends React.Component
 
 function reset()
 {
-    //BubbleSort();
     generateNewArray();
+    console.log('reeeeeeeeeeeeeeeeeeeeeeset');
     vector.map((value,index) =>
     {
         let bar = document.getElementsByClassName("bars");
@@ -96,9 +99,88 @@ function BubbleSort()
  
 }
 
+function afis()
+{
+    for(let i=0;i<vector.length;i++)
+    {
+        let bar=document.getElementsByClassName("bars");
+        bar[i].style.height=Math.floor(vector[i]*((wheight)*65/100)/100)+'px';
+        console.log(vector[i]);
+    }
+}
+
 window.onload = function()
 {
     reset();
+}
+
+let secundar = [];
+
+function merge(array,b,s,d,m)
+{
+    let i=s;
+    let j=m+1;
+    let k=s;
+
+    while(i<=m && j<=d)
+    {
+        if(array[i]<=array[j]) 
+        {
+            b[k++]=array[i++];
+            compara.push(k-1,b[k-1]);
+        }
+        else 
+        {
+            b[k++]=array[j++];
+            compara.push(k-1,b[k-1]);
+        }
+    }
+    while(i<=m) 
+    {
+        b[k++]=array[i++];
+        compara.push(k-1,b[k-1]);
+    }
+    while(j<=d) 
+    {
+        b[k++]=array[j++];
+        compara.push(k-1,b[k-1]);
+    }
+    for(i=s;i<=d;i++) array[i]=b[i];
+}
+
+function MergeSortMic(array,b,s,d)
+{
+    if((d-s)<=1) 
+    {
+        if(array[s]>array[d]) 
+        {
+            let aux=array[s];
+            array[s]=array[d];
+            array[d]=aux;
+        }
+    }
+    else
+    {
+        let m = Math.floor((s+d)/2);
+        MergeSortMic(array,b,s,m);
+        MergeSortMic(array,b,m+1,d);
+        merge(array,b,s,d,m);
+    }
+}
+
+function MergeSort()
+{
+    compara = [];
+    MergeSortMic(vector,secundar,0,vector.length-1);
+
+    let bar = document.getElementsByClassName("bars");
+    for(let i=0;i<compara.length-1;i+=2)
+    {
+        setTimeout(() => {
+            let x = Math.floor(compara[i+1]*((wheight)*65/100)/100);
+            bar[compara[i]].style.height = x+'px';
+        },i*7);
+    }
 }
 
 export default Element;
